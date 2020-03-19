6ebd232f184238eddfcc5c97cc1f186d55099d8e
Collin Jilbert <cjilbert504@gmail.com>
2020-03-18 19:00:12 -0500

    add private method set todo list and add before action to do set todo list private method

--- app/controllers/todo_items_controller.rb
+++ app/controllers/todo_items_controller.rb
@@ -1,2 +1,12 @@
 class TodoItemsController < ApplicationController
+
+    before_action :set_todo_list
+
+    private
+        
+        def set_todo_list
+            @todo_list = TodoList.find(params[:todo_list_id])
+        end
+
+        
 end
